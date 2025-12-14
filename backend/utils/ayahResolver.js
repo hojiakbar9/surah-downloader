3d8be5fca0a03472a1eca7bd7e7bcf9031fd0058
Khayitboev Khojiakbar <xayitbaevhojiakbar@gmail.com>
2025-12-14 16:29:58 +0100

    feat: add dotenv for environment variable management and implement ayah resolution function

---
+++ backend/utils/ayahResolver.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const url = process.env.BASE_AUDIO_URL + '/Minshawy_Murattal_128kbps';
+const audioFormat = process.env.AUDIO_FORMAT;
+
+function resolveAyah(surahNumber, ayahNumber){
+    let surahNumberThreeDigit =  surahNumber.toString().padStart(3, '0');
+    let ayahNumberThreeDigit =  ayahNumber.toString().padStart(3, '0');
+
+
+    return `${url}/${surahNumberThreeDigit}${ayahNumberThreeDigit}.${audioFormat}`;
+}
